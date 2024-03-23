561591d8d3316b3868d1e19642eee9417b2c99ad
xikmatilla6229 <xikmatilla996@gmail.com>
2024-03-23 16:01:01 +0500

    dondon-ziki

---
+++ suxrob.js
@@ -0,0 +1,16 @@
+// let a =['olma, uzum, nok']
+// let b=confirm(a)
+// if(b==true) {
+//     a.push('uzum market');
+//     console.log();
+
+// }
+
+let a=["bmw","mers","audi","nexia 2 legend"]
+let b=confirm(a);
+if (b==true){
+    a.pop("bmw")
+}
+else if(b==false){
+}console.log(a);
+alert(a)
